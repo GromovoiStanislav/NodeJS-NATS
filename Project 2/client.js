c48b68fa62075afb53280fcf676ee355f37d2fb0
Gromovoi Stanislav <gromila12@rambler.ru>
2024-01-04 11:21:50 +0600

    Project 2

---
+++ Project 2/client.js
@@ -0,0 +1,39 @@
+import { connect, Empty, StringCodec } from 'nats';
+
+// create a connection
+const nc = await connect({ servers: 'demo.nats.io:4222' });
+
+// create an encoder
+const sc = StringCodec();
+
+// the client makes a request and receives a promise for a message
+// by default the request times out after 1s (1000 millis) and has
+// no payload.
+await nc
+  .request('time', Empty, { timeout: 1000 })
+  .then((m) => {
+    console.log(`got response: ${sc.decode(m.data)}`);
+  })
+  .catch((err) => {
+    console.log(`problem with request: ${err.message}`);
+  });
+
+await nc
+  .request('admin.uptime', Empty, { timeout: 1000 })
+  .then((m) => {
+    console.log(`got response: ${sc.decode(m.data)}`);
+  })
+  .catch((err) => {
+    console.log(`problem with request: ${err.message}`);
+  });
+
+await nc
+  .request('admin.stop', Empty, { timeout: 1000 })
+  .then((m) => {
+    console.log(`got response: ${sc.decode(m.data)}`);
+  })
+  .catch((err) => {
+    console.log(`problem with request: ${err.message}`);
+  });
+
+await nc.close();
