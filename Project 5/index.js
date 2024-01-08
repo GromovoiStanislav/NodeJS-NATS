79e5bd2064a3d441508a62371c9fa37ce5628c9e
Gromovoi Stanislav <gromila12@rambler.ru>
2024-01-08 14:30:36 +0600

    Project 5

---
+++ Project 5/index.js
@@ -0,0 +1,28 @@
+import { connect } from 'nats';
+
+// to create a connection to a nats-server:
+const nc = await connect({ servers: 'demo.nats.io:4222' });
+
+const sub = await nc.subscribe('foo', { max: 2 });
+(async () => {
+  for await (const m of sub) {
+    try {
+      const obj = m.json();
+      console.log(obj);
+      console.log(obj.foo, obj.bar);
+    } catch (err) {
+      console.log(`err: ${err.message}: '${m.string()}'`);
+    }
+  }
+})();
+
+nc.publish('foo', JSON.stringify({ foo: 'bar', bar: 27 }));
+nc.publish('foo', 'not json');
+nc.publish('foo', JSON.stringify({ foo: 'bar', bar: 28 }));
+
+await nc.drain();
+
+// Output:
+// { foo: 'bar', bar: 27 }
+// bar 27
+// err: Bad JSON: 'not json'
